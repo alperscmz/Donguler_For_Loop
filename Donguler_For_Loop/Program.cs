71b5e2382113238e4ac497963bdd8e1738a96a41
alperscmz <alpersacmaozu@gmail.com>
2022-01-22 20:10:22 +0300

    Donguler_For_Loop
    vsc c# ile Donguler_For_Loop

---
+++ Donguler_For_Loop/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace Donguler_For_Loop
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            int sayac;
+          Console.WriteLine("Sayi giriniz:");
+          sayac=Convert.ToInt32(Console.ReadLine());
+          for (int i = 1; i<=sayac; i++)
+          {
+              if(i%2==1)
+                Console.WriteLine(i);
+          }
+          
+
+          int tektoplam=0;
+          int cifttoplam=0; 
+          for (int i = 0; i <=1000 ; i++)
+          {
+              if(i%2==1)
+              tektoplam=tektoplam+i;
+              else
+              cifttoplam=cifttoplam+i;
+          }
+          Console.WriteLine("Tek sayıların toplamı: " +tektoplam);
+          Console.WriteLine("Çift sayıların toplamı: " +cifttoplam);
+
+
+          for (int i = 0; i <10 ; i++)
+          {
+              if(i==4)
+              break;
+              Console.WriteLine(i);
+          }
+          
+          for (int i = 0; i <10 ; i++)
+          {
+              if(i==4)
+              continue;
+              Console.WriteLine(i);
+          }
+
+           
+        }
+    }
+}
+
